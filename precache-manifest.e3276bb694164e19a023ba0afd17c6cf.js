ca239b39cc1be67ffff3c75cb17cf9405032f5fa
ahujadheeraj <ahujadheeraj99@gmail.com>
2019-08-17 20:30:49 +0530

    Updates

---
+++ precache-manifest.e3276bb694164e19a023ba0afd17c6cf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e86cea0f2dcfb80bc82d5811a0919e",
+    "url": "/Homepage/index.html"
+  },
+  {
+    "revision": "03d7ab4f7c8d7ef76656",
+    "url": "/Homepage/static/css/main.e28f3812.chunk.css"
+  },
+  {
+    "revision": "ee0bde10505b96ce5ab2",
+    "url": "/Homepage/static/js/2.5d6d3ca4.chunk.js"
+  },
+  {
+    "revision": "03d7ab4f7c8d7ef76656",
+    "url": "/Homepage/static/js/main.83429ce2.chunk.js"
+  },
+  {
+    "revision": "bbf7f48458c087099da2",
+    "url": "/Homepage/static/js/runtime~main.0c3732f2.js"
+  },
+  {
+    "revision": "f1be2b4434e6fd27b8b154f572661dd7",
+    "url": "/Homepage/static/media/adt.f1be2b44.svg"
+  },
+  {
+    "revision": "7c01bfc046a029227993df347594bc58",
+    "url": "/Homepage/static/media/flipkart.7c01bfc0.svg"
+  },
+  {
+    "revision": "77d9431cbbe2c17b303a62efcfc17d20",
+    "url": "/Homepage/static/media/honda.77d9431c.svg"
+  },
+  {
+    "revision": "e19de7ae92c716fac12cd45b02ff8c3a",
+    "url": "/Homepage/static/media/playment.e19de7ae.svg"
+  },
+  {
+    "revision": "091d3fcc72ab5c5ead2cb1fe060ed23f",
+    "url": "/Homepage/static/media/quotes.091d3fcc.svg"
+  },
+  {
+    "revision": "bf876d81f291406719fc6adcc006eab9",
+    "url": "/Homepage/static/media/yourstory.bf876d81.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.cbbdc9ee9d23a77e4e729d35a6f131bd.js"
+  "/Homepage/precache-manifest.e3276bb694164e19a023ba0afd17c6cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Homepage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
